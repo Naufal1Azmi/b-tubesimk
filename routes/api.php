72e1dcff43c23a498f75ef79e7cdc692c2498ade
NaufalAzmiLubis <m.naufalazmilubis@gmail.com>
2024-05-16 17:06:11 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+//route login
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
+
+//group route with middleware "auth"
+Route::group(['middleware' => 'auth:api'], function() {
+
+    //logout
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+    
+});
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    //group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+        
+        //dashboard
+        Route::get('/dashboard', App\Http\Controllers\Api\Admin\DashboardController::class);
+        
+        //permissions
+        Route::get('/permissions', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'index'])
+        ->middleware('permission:permissions.index');
+
+        //permissions all
+        Route::get('/permissions/all', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'all'])
+        ->middleware('permission:permissions.index');
+
+        //roles all
+        Route::get('/roles/all', [\App\Http\Controllers\Api\Admin\RoleController::class, 'all'])
+        ->middleware('permission:roles.index');
+
+        //roles
+        Route::apiResource('/roles', App\Http\Controllers\Api\Admin\RoleController::class)
+        ->middleware('permission:roles.index|roles.store|roles.update|roles.delete');
+
+        //users
+        Route::apiResource('/users', App\Http\Controllers\Api\Admin\UserController::class)
+        ->middleware('permission:users.index|users.store|users.update|users.delete');
+
+        //categories all
+        Route::get('/categories/all', [\App\Http\Controllers\Api\Admin\CategoryController::class, 'all'])
+        ->middleware('permission:categories.index');
+
+        //Categories
+        Route::apiResource('/categories', App\Http\Controllers\Api\Admin\CategoryController::class)
+        ->middleware('permission:categories.index|categories.store|categories.update|categories.delete');
+
+    });
+});
